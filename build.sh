96fe667fa01c8cef44d4d33704cc7b83b2c74fe0
Mori <154768101+moriT958@users.noreply.github.com>
2024-08-25 14:41:09 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-codon build -release -o codon_bin/random_nums ./src/codon_server/random_nums.py
+if [ ! -e codon_bin/random_nums ]; then
+  codon build -release -o codon_bin/random_nums ./src/codon_server/random_nums.py
+fi
 
 rye run ruff format
 rye run ruff check
